d5a0c0d3032a29511df0ba95354f7c98bff801b4
Mashrabboy <m.akbarov94@gmail.com>
2016-03-29 10:07:05 -0700

    Update mybot.php

--- mybot.php
+++ mybot.php
@@ -1,7 +1,7 @@
 <?php 
 $botToken = "218506042:AAEG9PXVBgH6AUtMYaZxnYuRnhJofGGPBuc";
 $website = "https://api.telegram.org/bot".$botToken;
-$update =file_get_contents("php://input");
+$update =file_get_contents($website."/getupdates");
 $updateArray = json_decode($update,TRUE);
 $chatId = $updateArray["result"][0]["message"] ["chat"]["id"];
 file_get_contents($website."/sendmessage?chat_id=".$chatId."&text=test");
