51418332e387236955f7e194a9bc075b3c44a5bf
Mashrabboy <m.akbarov94@gmail.com>
2016-03-29 04:08:27 -0700

    Update mybot.php

--- mybot.php
+++ mybot.php
@@ -1,1 +1,8 @@
-
+<?php 
+$botToken = "218506042:AAEG9PXVBgH6AUtMYaZxnYuRnhJofGGPBuc";
+$website = "https://api.telegram.org/bot".$botToken;
+$update =file_get_contents("php://input");
+$updateArray = json_decode($update,TRUE);
+$chatId = $updateArray["result"][0]["message"] ["chat"]["id"];
+file_get_contents($website."/sendmessage?chat_id=".$chatId."&text=test");
+?>
